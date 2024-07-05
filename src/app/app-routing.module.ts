a01741b67188d07b31b235022834c8fd9c006076
Abhishek Samanta <Abhishek.Samanta@example.com>
2024-07-05 20:33:26 +0530

    Angular18 Server Side Rendering(SSR)

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Demo1Component } from './demo1/demo1.component';
+import { Demo2Component } from './demo2/demo2.component';
+
+const routes: Routes = [
+  {path:'demo1',component:Demo1Component},
+  {path:'demo2',component:Demo2Component},
+  {path:'',redirectTo:'/demo1',pathMatch:'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
